695fcc5aa629d22d5ce1e6346e1bcc98346d8356
Christian Schwarz <cmrs@cmrs.io>
2019-03-15 17:35:08 +0100

    use utf-8 encoding to add support for unicodes etc.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib.parse import parse_qs
-
+from urllib.parse import parse_qs, quote
 
 urls = {}
-with open("urls.list", "r") as fd:
+with open("urls.list", "r", encoding="utf-8") as fd:
 	for line in fd.readlines():
 		split = line.split(" ")
 		if len(split) != 2:
@@ -11,10 +10,11 @@ with open("urls.list", "r") as fd:
 
 		path = split[0]
 		url = split[1]
+		url = url[0 : len(url) - 1]
 
 		urls[path] = url
 
-fd = open("urls.list", "a")
+fd = open("urls.list", "a", encoding="utf-8")
 
 ADDR = "0.0.0.0"
 PORT = 8080
@@ -26,14 +26,15 @@ class RequestHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		if self.path in urls:
 			self.send_response(302)
-			self.send_header("Location", urls[self.path])
+			self.send_header('Content-type', 'text/text; charset=utf-8')
+			self.send_header("Location", quote(urls[self.path], safe="/:."))
 			self.end_headers()
 			self.send("")
 		else:
 			self.send_response(404)
 			self.end_headers()
 			self.send("Invalid path " + self.path)
-			
+
 	def do_POST(self):
 		length = int(self.headers["Content-Length"])
 		data = parse_qs(self.rfile.read(length).decode("utf-8"))
