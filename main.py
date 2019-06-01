aa8b28107a74f6e5b3615228eed1aeb58a64b932
Jakob Löw <jakob@m4gnus.de>
2019-06-01 18:40:41 +0200

    fix some security issues and allow subdomains to act like shortens

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qs, quote
+import re, json
 
 urls = {}
 with open("urls.list", "r", encoding="utf-8") as fd:
@@ -11,10 +12,13 @@ with open("urls.list", "r", encoding="utf-8") as fd:
 		path = split[0]
 		url = split[1]
 		url = url[0 : len(url) - 1]
+		url = json.loads(url)
 
 		urls[path] = url
 
 fd = open("urls.list", "a", encoding="utf-8")
+
+reg = re.compile("^/?\w.*$")
 
 ADDR = "0.0.0.0"
 PORT = 8080
@@ -23,18 +27,25 @@ class RequestHandler(BaseHTTPRequestHandler):
 	def send(self, msg):
 		self.wfile.write(msg.encode("utf-8"))
 
+	def redirectTo(self, url):
+		self.send_response(302)
+		self.send_header('Content-type', 'text/text; charset=utf-8')
+		self.send_header("Location", quote(url, safe="/:."))
+		self.end_headers()
+		self.send("")
+
 	def do_GET(self):
+		lowest_domain = self.headers["host"].split(".")[0]
+
 		if self.path == "/url-shortener":
 			self.send_response(200)
 			self.end_headers()
 			with open("interface.html", "r") as fd:
 				self.send(fd.read())
+		elif self.path == "/" and lowest_domain in urls:
+			self.redirectTo(urls[lowest_domain])
 		elif self.path in urls:
-			self.send_response(302)
-			self.send_header('Content-type', 'text/text; charset=utf-8')
-			self.send_header("Location", quote(urls[self.path], safe="/:."))
-			self.end_headers()
-			self.send("")
+			self.redirectTo(urls[self.path])
 		else:
 			self.send_response(404)
 			self.end_headers()
@@ -44,14 +55,16 @@ class RequestHandler(BaseHTTPRequestHandler):
 		length = int(self.headers["Content-Length"])
 		data = parse_qs(self.rfile.read(length).decode("utf-8"))
 
-		if "path" in data and "url" in data and data["path"] != "/" and data["path"][0][1] != "." and data["path"][0][0] != ".":
+		print(data)
+
+		if "path" in data and "url" in data and reg.match(data["path"][0]) is not None:
 			path = data["path"][0]
-			url = data["url"][0].replace(" ", "+")
+			url = data["url"][0]
 			if path[0] != "/":
 				path = "/" + path
 
 			urls[path] = url
-			fd.write("{} {}\n".format(path, url))
+			fd.write("{} {}\n".format(path, json.dumps(url)))
 			fd.flush()
 
 			self.send_response(200)
